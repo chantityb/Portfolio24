23afbbdbebd30a8def528daa32031bb55ef00816
Chantity <chantityb@gmail.com>
2024-04-16 16:56:44 -0400

    moved project images

--- src/sections/Portfolio/projects.js
+++ src/sections/Portfolio/projects.js
@@ -1,26 +1,33 @@
+import tictac from "./images/tic-tac.png";
+import movie from "./images/movie.png"; 
+import backroads from "./images/backroads.png"; 
+import pushing from "./images/pf.jpg"; 
+
+
+
 export const projects = [
   {
     title: "Tic Tac Toe",
     description: "The project is an alternative version of the tic tac toe game created using React.js. This app also uses CSS grid layout and JS audio functionality.",
-    image: "/src/assets/tic-tac.png",
+    image: tictac,
     url: "https://react-tactoe.netlify.app",
   },
   {
     title: "Movie App",
     description: "This project is a movie app created using React.js that fetches data from the OMDb API with search functionality",
-    image: "/src/assets/movie.png",
+    image: movie,
     url: "https://rctjs-movie-app.netlify.app",
   },
   {
     title: "Tour App",
     description: "This project is a simple landing page for a travel agency using React.js.",
-    image: "/src/assets/backroads.png",
+    image: backroads,
     url: "https://react-backroads-tour.netlify.app/",
   },
   {
     title: "Pushing Forward",
     description: "This project was created using WordPress as the CMS and Divi as the theme. The site consists of custom CSS and Vanilla JS throughout ",
-    image: "/src/assets/pf.jpg",
+    image: pushing,
     url: "https://pushingforwardinc-com.stackstaging.com/",
   },
   
