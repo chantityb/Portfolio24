da24d638433c1f2f50ba29b6675ee21983356c5b
Chantity <chantityb@gmail.com>
2024-05-22 15:10:18 -0400

    added car dealership project

--- src/sections/Portfolio/projects.js
+++ src/sections/Portfolio/projects.js
@@ -2,6 +2,7 @@ import tictac from "./images/tic-tac.png";
 import movie from "./images/movie.png"; 
 import backroads from "./images/backroads.png"; 
 import pushing from "./images/pf.jpg"; 
+import dealership from "./images/dealership.png"
 
 
 
@@ -13,22 +14,28 @@ export const projects = [
     url: "https://react-tactoe.netlify.app",
   },
   {
-    title: "Movie App",
-    description: "This project is a movie app created using React.js that fetches data from the OMDb API with search functionality",
-    image: movie,
-    url: "https://rctjs-movie-app.netlify.app",
-  },
-  {
     title: "Tour App",
     description: "This project is a simple landing page for a travel agency using React.js.",
     image: backroads,
     url: "https://react-backroads-tour.netlify.app/",
   },
   {
+    title: "Movie App",
+    description: "This project is a movie app created using React.js that fetches data from the OMDb API with search functionality",
+    image: movie,
+    url: "https://rctjs-movie-app.netlify.app",
+  },
+  {
     title: "Pushing Forward",
-    description: "This project was created using WordPress as the CMS and Divi as the theme. The site consists of custom CSS and Vanilla JS throughout ",
+    description: "This project was created using WordPress as the CMS and Divi as the theme. The site consists of custom CSS and Vanilla JS throughout",
     image: pushing,
     url: "https://pushingforwardinc-com.stackstaging.com/",
+  },
+  {
+    title: "Car Dealership",
+    description: "This project is car dealership that loads cars as well as featured cars dynamically using Vanilla JS",
+    image: dealership,
+    url: "https://car-dealership-javascript.netlify.app/",
   },
   
 ]
